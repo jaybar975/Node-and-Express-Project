da0dd1698798765e7bafbdd4c964847d7a2a14e8
Justin Aybar <jaybar975@west-mec.org>
2019-10-08 15:15:49 -0700

    Started Project
    Almost Done

---
+++ Projects/login-portal/app.js
@@ -0,0 +1,83 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+const dashboardRouter = require("./routes/dashboard");         
+const publicRouter = require("./routes/public");
+
+var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: '{https://dev-838117.okta.com}',
+  token: '{00UgyWgSUU8tTcCz4UzfPyMiw8LcFRp2m-sxBMgMIQ}'
+});
+const oidc = new ExpressOIDC({
+  issuer: "{https://dev-838117.okta.com}/oauth2/default",
+  client_id: {yourClientId},
+  client_secret: {yourClientSecret},
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
+
+app.use(session({
+  secret: 'asdf;lkjh3lkjh235l23h5l235kjh',
+  resave: true,
+  saveUninitialized: false
+}));
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', publicRouter);
+app.use('/dashboard', dashboardRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
